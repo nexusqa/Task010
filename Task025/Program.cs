f86dba5410a24b842e62721c38bd409bb4739ec5
anton <anton@home.ru>
2023-11-01 13:20:42 +0300

    add new tasks

---
+++ Task025/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+using System;
+using static System.Console;
+
+Clear();
+
+int A = int.Parse(ReadLine());
+int B = int.Parse(ReadLine());
+
+WriteLine($"Число {A} в степени {B} равно {GetStepen(A,B)}");
+
+int GetStepen(int x, int y)
+{
+    int num = Convert.ToInt32(Math.Pow(x, y));
+    return num;
+}
---
+++ Task027/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+WriteLine("Введите число: ");
+int x = int.Parse(ReadLine());
+
+WriteLine($"Сумма чисел {x} равна {GetSummIn(x)}");
+
+
+
+
+int GetSummIn(int number)
+{
+    int summ = 0;
+    while(number != 0)
+    {
+        summ = summ + number%10;
+        number = number/10;
+    }
+    return summ;
+
+}
